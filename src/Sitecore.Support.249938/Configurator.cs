14429b6cfd3710977dde160d1fd919ab27aff8d2
Vladimir Taran <vlt@sitecore.net>
2018-09-06 15:08:01 -0400

    Change the patch architecture to less intersect with the default functional.

--- src/Sitecore.Support.249938/Configurator.cs
+++ src/Sitecore.Support.249938/Configurator.cs
@@ -1,14 +1,8 @@
 ﻿// © 2017 Sitecore Corporation A/S. All rights reserved. Sitecore® is a registered trademark of Sitecore Corporation A/S.
 
 using Microsoft.Extensions.DependencyInjection;
-using Sitecore.Content.Services.Items.OData;
-using Sitecore.Content.Services.Items.OData.Search;
-using Sitecore.ContentSearch.Utilities;
 using Sitecore.DependencyInjection;
-using Sitecore.Services.Infrastructure.Sitecore.Data;
-using Sitecore.Services.Infrastructure.Sitecore.DependencyInjection;
-using Sitecore.Services.Infrastructure.Sitecore.Services;
-using Sitecore.Content.Services;
+using System.Linq;
 
 namespace Sitecore.Support.Content.Services
 {
@@ -16,22 +10,13 @@ namespace Sitecore.Support.Content.Services
   {
     public void Configure(IServiceCollection serviceCollection)
     {
-      var assemblies = new[] { typeof(Sitecore.Content.Services.Configurator).Assembly };
-      serviceCollection.AddWebApiControllers(assemblies);
-
-      serviceCollection
-          .AddScoped<IItemRepository, ItemRepository>()
-          .AddScoped<IItemDataModelFactory, ItemDataModelFactory>()
-          .AddScoped<ApiKeyHelper>()
-          .AddScoped<ContentSearchManagerWrapper>()
-          .AddScoped<QueryBuilder>()
-          .AddScoped<Sitecore.Content.Services.Items.OData.Search.FilterBinder, Sitecore.Suport.Content.Services.Items.OData.Search.FilterBinder>()
-          .AddScoped<ODataItemSearch>()
-          .AddScoped<ComparisonExpressionBuilder>()
-          .AddScoped<FieldNameResolver>()
-          .AddScoped<OrderByBinder>()
-          .AddScoped<Sitecore.Content.Services.Items.OData.Search.SearchHelper>()
-          .AddSingleton<ItemServiceDescriptor>();
+      var descriptor = serviceCollection.FirstOrDefault(d => d.ServiceType == typeof(Sitecore.Content.Services.Items.OData.Search.FilterBinder));
+      if (descriptor != null)
+      {
+        serviceCollection.Remove(descriptor);
+      }
+      serviceCollection.AddSingleton<Sitecore.Content.Services.Items.OData.Search.FilterBinder, Sitecore.Suport.Content.Services.Items.OData.Search.FilterBinder>();
     }
   }
 }
+
